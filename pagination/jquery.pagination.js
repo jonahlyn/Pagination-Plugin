7485e5eb7d615ea2e53ddd66d1ec55fe10670bff
jonahlyn <jgilstrap@gmail.com>
2011-02-10 16:01:23 -0700

    Fixed bugs where elements that were paginated were not independent of one another in terms of itemsPerPage and navigation emphasis.

--- pagination/jquery.pagination.js
+++ pagination/jquery.pagination.js
@@ -6,17 +6,15 @@
 
 (function($){
 	
-	var opts;
-	
 	$.fn.pagination = function(options){
-		opts = $.extend({}, $.fn.pagination.defaults, options);
+		var opts = $.extend({}, $.fn.pagination.defaults, options);
 		
 		return this.each(function(){
 			var page_elem = $(this),
 				items = page_elem.children('li'), 
 				total_items = items.length, 
 				total_pages = Math.ceil(total_items / opts.itemsPerPage),
-				nav;
+				nav, nav2;
 			
 			function displayPage(pageNum){
 				var current_page = pageNum || 1,					
@@ -26,7 +24,16 @@
 				items.hide();
 				items.slice(low,high).show();
 				emphasizeCurrentPage(current_page);
-			};
+			}
+			
+			function emphasizeCurrentPage(current_page){
+				var nav_index = (current_page - 1) || 0;
+				$.each([nav, nav2], function(idx, val){
+					$('li a', $(val))
+						.css({'font-weight':'normal'})
+						.filter(':eq('+nav_index+')').css({'font-weight':'bold'});					
+				});				
+			}
 			
 			displayPage();
 			
@@ -34,29 +41,22 @@
 			$('li a',nav).click(function(e){
 				e.preventDefault();
 				displayPage(e.target.textContent);
-			});			
-			page_elem.before(nav).after(nav.clone(true));
+			});
+			nav2 = nav.clone(true);
+			page_elem.before(nav).after(nav2);
+			
 			emphasizeCurrentPage();
 		});
 	};
 	
 	function buildNavigation(total_pages){
-		var nav = $('<ul class="'+opts.navigationClass+'"></ul>'), i = 1;
+		var nav = $('<ul class="pagination"></ul>'), i = 1;
 		
 		for(;i<=total_pages;i++){
 			nav.append('<li><a href="#">'+i+'</a></li>');
 		}
 		
 		return nav;
-	}
-	
-	function emphasizeCurrentPage(current_page){
-		var nav_index = (current_page - 1) || 0;
-		$('ul.'+opts.navigationClass).each(function(){
-			$('li a',$(this))
-				.css({'font-weight':'normal'})
-				.filter(':eq('+nav_index+')').css({'font-weight':'bold'});
-		});
 	}
 	
 	$.fn.pagination.defaults = {
